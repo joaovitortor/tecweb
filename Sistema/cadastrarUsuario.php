7675843ec48777a3cd82a6ebcc15d0dee3941aab
João Vitor Bidoia Angelo <joaovitorbidoiaangelo@gmail.com>
2023-09-15 11:54:11 -0300

    cadastro 1 *

--- Sistema/cadastrarUsuario.php
+++ Sistema/cadastrarUsuario.php
@@ -17,10 +17,10 @@ if (isset($_POST['salvar'])) {
     $nome = $_POST['nome'];
     $email = $_POST['email'];
     $senha = $_POST['senha'];
-    $usuariogrupo_id = $_POST['usuariogrupo_id']
+    $usuariogrupo_id = $_POST['usuariogrupo_id'];
 
     //3. Preparar a SQL
-    $sql = "insert into usuario (nome, email, senha, arquivo, usuariogrupo_id) values ('$nome', '$email', '$senha', '$nomeArquivo', '$usuariogrupo_id)";
+    $sql = "insert into usuario (nome, email, senha, arquivo, usuariogrupo_id) values ('$nome', '$email', '$senha', '$nomeArquivo', '$usuariogrupo_id')";
 
     //4. Executar a SQL
     mysqli_query($conexao, $sql);
